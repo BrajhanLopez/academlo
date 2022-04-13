6cc5e02a256ab67087c2bc6e73da37fa3cad9fa1
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-04-13 00:57:36 -0500

    PORTAFOLIO TERMINADO V1.0

--- portafolio/assets/js/funtions.js
+++ portafolio/assets/js/funtions.js
@@ -1,6 +1,19 @@
 const boton=document.querySelector('.button')
 const nav= document.querySelector('.ul')
 
+const li =document.querySelectorAll('.li')
+
 boton.addEventListener('click',()=>{
-nav.classList.toggle('activarboton')
+    nav.classList.toggle('activarboton')
+    
 })
+
+
+
+li.forEach(lista =>{
+    lista.addEventListener('click',function(){
+        nav.classList.toggle('activarboton')
+    })
+})
+
+
